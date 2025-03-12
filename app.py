7addc2e4707eea0bdbf8b68bc87f1f5e9c3f9204
Arnav Sagar <thearnav001@gmail.com>
2025-03-13 01:35:02 +0530

    Added new columns to database

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ db=SQLAlchemy(app)
 class SBMS(db.Model):
     rollno=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(200), nullable=False)
-    desc=db.Column(db.String(500), nullable=False)
+    fname=db.Column(db.String(200), nullable=False)
+    mname=db.Column(db.String(200), nullable=False)
+    gender=db.Column(db.String(20), nullable=False)
+    dob=db.Column(db.String(200), nullable=False)
+    course=db.Column(db.String(100), nullable=False)
+    branch=db.Column(db.String(100), nullable=False)
+    email=db.Column(db.String(200), nullable=False)
+    phone=db.Column(db.String(15), nullable=False)
+    gradyear=db.Column(db.Integer, nullable=False)
+    add=db.Column(db.String(500), nullable=False)
     date_created=db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self)->str:
@@ -21,8 +30,17 @@ def hello_world():
     if request.method == 'POST':
         rollno = request.form["roll"]
         name = request.form["name"]
-        desc = request.form["desc"]
-        sbms = SBMS(rollno=rollno, name=name, desc=desc)
+        fname = request.form["fname"]
+        mname = request.form["mname"]
+        gender = request.form["gender"]
+        dob = request.form["dob"]
+        course = request.form["course"]
+        branch = request.form["branch"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        gradyear = request.form["gradyear"]
+        add = request.form["add"]
+        sbms = SBMS(rollno=rollno, name=name,fname=fname, mname=mname, gender=gender, dob=dob, course=course, branch=branch, email=email, phone=phone, gradyear=gradyear,  add=add)
         db.session.add(sbms)
         db.session.commit()
 
@@ -41,11 +59,29 @@ def update(rollno):
     if request.method=='POST':
         rollno = request.form["roll"]
         name = request.form["name"]
-        desc = request.form["desc"]
+        fname = request.form["fname"]
+        mname = request.form["mname"]
+        gender = request.form["gender"]
+        dob = request.form["dob"]
+        course = request.form["course"]
+        branch = request.form["branch"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        gradyear = request.form["gradyear"]
+        add = request.form["add"]
         sbms=SBMS.query.filter_by(rollno=rollno).first()
         sbms.rollno=rollno
         sbms.name=name
-        sbms.desc=desc
+        sbms.fname=fname
+        sbms.mname=mname
+        sbms.gendergender=name
+        sbms.dob=dob
+        sbms.course=course
+        sbms.branch=branch
+        sbms.email=email
+        sbms.phone=phone
+        sbms.gradyear=gradyear
+        sbms.add=add
         db.session.add(sbms)
         db.session.commit()
         return redirect("/")
